5e1c2243b3475c906dea3a7a36ace5f438caee17
Wyatt Murray <wyattmurray1235@gmail.com>
2024-11-09 04:32:03 -0500

    added box colliders to define walls, water, and redefined tank collider to mesh better with blocky surface

--- Assets/Scripts/Scene Management/LobbySceneManager.cs
+++ Assets/Scripts/Scene Management/LobbySceneManager.cs
@@ -1,9 +1,27 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class LobbySceneManager : NetworkBehaviour
 {
     //level selection
     //map selection
     //migrate all players (maybe call respawn on all of them?)
+
+    //put all
+
+    //client stuff
+
+    void NotifyClientOfSceneChange()
+    {
+
+    }
+    [ClientRpc]
+    void sceneChangeClientRPC()
+    {
+        //tell client to fade out, and wait for fade in call
+        
+    }
+
+    //server stuff
 }
